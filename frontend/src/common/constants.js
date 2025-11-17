39990b7018aff4891597a91cc2cc89f549c6e1bb
haijin <haijin736@google.com>
2025-11-17 18:22:20 +0900

    add back constants.js

---
+++ frontend/src/common/constants.js
@@ -0,0 +1,6 @@
+export const REGION = "us-east-1";
+export const APP_NAME = "Automated Claims Processing";
+export const USE_BROWSER_ROUTER = true;
+export const API_ENDPOINT = "https://xxxxxxxx.execute-api.us-east-1.amazonaws.com/dev/";
+export const COGNITO_USER_POOL_ID = "us-east-1_xxxxxxx";
+export const COGNITO_USER_POOL_CLIENT_ID = "xxxxxxxxxxxxxx";
